b91cbd9db8717ec7493ca7a936378830d66ee1b0
Daniel Wehner <dawehner@googlemail.com>
2017-10-09 17:35:31 +0100

    Add a billing cycle field type

---
+++ src/Plugin/Field/FieldWidget/BillingCycle.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Drupal\commerce_recurring\Field\FieldWidget;
+
+use Drupal\Core\Field\FieldItemListInterface;
+use Drupal\Core\Field\WidgetBase;
+use Drupal\Core\Form\FormStateInterface;
+
+/**
+ * Plugin implementation of the 'commerce_billing_cycle' widget.
+ *
+ * @FieldWidget(
+ *   id = "commerce_billing_cycle_default",
+ *   label = @Translation("Billing cycle"),
+ *   field_types = {
+ *     "commerce_billing_cycle"
+ *   },
+ *  )
+ */
+class BillingCycle extends WidgetBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function formElement(FieldItemListInterface $items, $delta, array $element, array &$form, FormStateInterface $form_state) {
+    $element['index'] += [
+      '#type' => 'number',
+      '#default_value' => $items->get($delta)->index,
+    ];
+    $element['start_date'] += [
+      '#type' => 'textfield',
+      '#default_value' => $items->get($delta)->start_date,
+    ];
+    $element['end_date'] += [
+      '#type' => 'textfield',
+      '#default_value' => $items->get($delta)->end_date,
+    ];
+
+    return $element;
+  }
+
+}
---
+++ test/src/Kernel/BillingCycleField.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace Drupal\Tests\commerce_recurring\Kernel;
+
+use Drupal\commerce_recurring\BillingCycle;
+use Drupal\Core\Datetime\DrupalDateTime;
+use Drupal\entity_test\Entity\EntityTest;
+use Drupal\field\Entity\FieldConfig;
+use Drupal\field\Entity\FieldStorageConfig;
+use Drupal\KernelTests\KernelTestBase;
+
+/**
+ * Tests the billing cycle field.
+ *
+ * @group commerce_recurring
+ */
+class BillingCycleField extends KernelTestBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public static $modules = ['commerce_recurring', 'field', 'entity_test', 'user'];
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function setUp() {
+    parent::setUp();
+
+    $this->installEntitySchema('user');
+    $this->installEntitySchema('entity_test');
+  }
+
+  public function testFieldItem() {
+    FieldStorageConfig::create([
+      'entity_type' => 'entity_test',
+      'field_name' => 'field_billing_cycle',
+      'type' => 'commerce_billing_cycle',
+    ])->save();
+
+    FieldConfig::create([
+      'entity_type' => 'entity_test',
+      'field_name' => 'field_billing_cycle',
+      'bundle' => 'entity_test',
+    ])->save();
+
+    $entity = EntityTest::create([
+      'field_billing_cycle' => [
+        'index' => 0,
+        'start_date' => '2017-10-09T15:07:12',
+        'end_date' => '2017-11-09T15:07:12',
+      ],
+    ]);
+    $entity->save();
+
+    $entity = EntityTest::load($entity->id());
+
+    /** @var \Drupal\commerce_recurring\BillingCycle $billing_cycle */
+    $billing_cycle = $entity->get('field_billing_cycle')->get(0)->toBillingCycle();
+    $this->assertInstanceOf(BillingCycle::class, $billing_cycle);
+    $this->assertEquals(0, $billing_cycle->getIndex());
+    $this->assertInstanceOf(DrupalDateTime::class, $billing_cycle->getStartDateTime());
+    $this->assertInstanceOf(DrupalDateTime::class, $billing_cycle->getEndDateTime());
+    $this->assertEquals('2017-10-09T15:07:12', $billing_cycle->getStartDateTime()->format('Y-m-d\TH:i:s'));
+    $this->assertEquals('2017-11-09T15:07:12', $billing_cycle->getEndDateTime()->format('Y-m-d\TH:i:s'));
+
+    // Make it possible to set date time objects
+    $entity->get('field_billing_cycle')->get(0)->setValue([
+      'start_date' => new \DateTime('2018-10-09T15:07:12'),
+      'end_date' => new \DateTime('2019-10-09T15:07:12'),
+    ]);
+
+    $billing_cycle = $entity->get('field_billing_cycle')->get(0)->toBillingCycle();
+    $this->assertInstanceOf(BillingCycle::class, $billing_cycle);
+    $this->assertEquals('2018-10-09T15:07:12', $billing_cycle->getStartDateTime()->format('Y-m-d\TH:i:s'));
+    $this->assertEquals('2019-10-09T15:07:12', $billing_cycle->getEndDateTime()->format('Y-m-d\TH:i:s'));
+  }
+}
